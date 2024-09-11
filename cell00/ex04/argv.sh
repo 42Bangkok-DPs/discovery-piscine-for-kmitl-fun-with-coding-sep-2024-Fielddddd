3a2f7e391b61fd6fe161df513d8b37d65d6e9a8b
Fielddddd <154392929+Fielddddd@users.noreply.github.com>
2024-09-12 00:57:28 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,1 +1,21 @@
+if [ $# -eq 0 ]
+then
+	echo "No arguments supplied"
+	exit 1
+fi
+
+if [ -n "$1" ]
+then
+	echo $1
+fi
+
+if [ -n "$2" ]
+then
+	echo $2
+fi
+
+if [ -n "$3" ]
+then
+	echo $3
+fi
 
